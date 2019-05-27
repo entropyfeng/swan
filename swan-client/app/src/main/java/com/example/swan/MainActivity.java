5dd4cf304227a91afc33781ad3bf7d47bddde32d
entropyfeng <1092758202@qq.com>
2019-05-27 20:00:35 +0800

    update

--- swan-client/app/src/main/java/com/example/swan/MainActivity.java
+++ swan-client/app/src/main/java/com/example/swan/MainActivity.java
@@ -5,17 +5,22 @@ import android.os.Bundle;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
 import com.amap.api.maps.model.MyLocationStyle;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private MapView mapView = null;
     private AMap aMap=null;
+    private UiSettings uiSettings=null;//定义一个UiSettings对象
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
         //获取地图控件引用
         mapView = (MapView) findViewById(R.id.main_map);
         //在activity执行onCreate时执行mapView.onCreate(savedInstanceState)，创建地图
@@ -24,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         if (aMap == null) {
             aMap = mapView.getMap();
             initMyLocation();
+
         }
     }
 
@@ -58,8 +64,16 @@ public class MainActivity extends AppCompatActivity {
         MyLocationStyle myLocationStyle;
         myLocationStyle = new MyLocationStyle();//初始化定位蓝点样式类myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）如果不设置myLocationType，默认也会执行此种模式。
         myLocationStyle.interval(2000); //设置连续定位模式下的定位间隔，只在连续定位模式下生效，单次定位模式下不会生效。单位为毫秒。
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_SHOW);//只定位一次。
         aMap.setMyLocationStyle(myLocationStyle);//设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true);//设置默认定位按钮是否显示，非必需设置。
         aMap.setMyLocationEnabled(true);// 设置为true表示启动显示定位蓝点，false表示隐藏定位蓝点并不进行定位，默认是false。
+        uiSettings = aMap.getUiSettings();//实例化UiSettings类对象
+        uiSettings.setCompassEnabled(true);
+        //aMap.setLocationSource(this);//通过aMap对象设置定位数据源的监听
+
+        uiSettings.setMyLocationButtonEnabled(true); //显示默认的定位按钮
+
+        aMap.setMyLocationEnabled(true);// 可触发定位并显示当前位置
     }
 }

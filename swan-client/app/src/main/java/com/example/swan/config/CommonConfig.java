7217c97ccee76979b740aa3e7fc97ef8c6d83460
entropyfeng <1092758202@qq.com>
2019-05-29 15:22:13 +0800

    Create CommonConfig.java
    添加配置文件

---
+++ swan-client/app/src/main/java/com/example/swan/config/CommonConfig.java
@@ -0,0 +1,13 @@
+package com.example.swan.config;
+
+/**
+ * @author feng
+ * 默认配置文件
+ *
+ */
+public class CommonConfig {
+
+
+    public static final String REMOTE="http://api.swan.shqawk.top";
+    public static final String LOCAL="http://127.0.0.1:8082";
+}

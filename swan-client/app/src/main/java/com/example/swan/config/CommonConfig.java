cb21119df1cc5a508bfa4e68284d49f9ed1a944a
entropyfeng <1092758202@qq.com>
2019-05-30 18:29:17 +0800

    add domain message and common config

--- swan-client/app/src/main/java/com/example/swan/config/CommonConfig.java
+++ swan-client/app/src/main/java/com/example/swan/config/CommonConfig.java
@@ -9,4 +9,5 @@ public class CommonConfig {
 
     public static final String REMOTE="http://api.swan.shqawk.top";
     public static final String LOCAL="http://127.0.0.1:8082";
+    public static final String URL=REMOTE;
 }
---
+++ swan-client/app/src/main/java/com/example/swan/domain/Message.java
@@ -0,0 +1,33 @@
+package com.example.swan.domain;
+
+import java.util.HashMap;
+
+public class Message {
+    private boolean success;
+    private String msg;
+    private HashMap<String,String> params=new HashMap<>();
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public HashMap<String, String> getParams() {
+        return params;
+    }
+
+    public void setParams(HashMap<String, String> params) {
+        this.params = params;
+    }
+}

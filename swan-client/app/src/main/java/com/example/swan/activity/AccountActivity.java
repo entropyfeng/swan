9902fdd99726a85e00596b8057d2fa30bf868077
entropyfeng <1092758202@qq.com>
2019-06-03 17:43:26 +0800

    add some activity

---
+++ swan-client/app/src/main/java/com/example/swan/activity/AccountActivity.java
@@ -0,0 +1,12 @@
+package com.example.swan.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class AccountActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ swan-client/app/src/main/java/com/example/swan/util/AccountHelper.java
@@ -0,0 +1,21 @@
+package com.example.swan.util;
+
+import android.content.Context;
+
+public class AccountHelper {
+
+
+    public static boolean isLogin(Context context){
+       return CommonUtil.isNullOrEmpty(readAuthToken(context));
+    }
+    public static void logout(Context context){
+        context.getSharedPreferences("data",Context.MODE_PRIVATE).edit().remove("auth_token").apply();
+
+    }
+    public static void writeAuthToken(Context context,String authToken){
+        context.getSharedPreferences("data",Context.MODE_PRIVATE).edit().putString("auth_token",authToken).apply();
+    }
+    public static String  readAuthToken(Context context){
+      return   context.getSharedPreferences("data",Context.MODE_PRIVATE).getString("auth_token","");
+    }
+}

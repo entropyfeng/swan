cb8fc0738a34a10e7089074f9f9764d6b2ffdad5
Devlegal <10703045@qq.com>
2019-06-03 10:37:15 +0800

    found the bug for BluePot, we need to go outside for using this function

--- swan-client/app/src/main/java/com/example/swan/FuncOfMap.java
+++ swan-client/app/src/main/java/com/example/swan/FuncOfMap.java
@@ -1,10 +1,20 @@
 package com.example.swan;
 
+import android.content.Intent;
+
 import com.amap.api.maps.AMap;
+import com.amap.api.maps.AMapOptions;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.CameraPosition;
+import com.amap.api.maps.model.LatLng;
 import com.amap.api.maps.model.MyLocationStyle;
+
+import static android.support.v4.content.ContextCompat.startActivity;
 
 public class FuncOfMap {
     private static MyLocationStyle myLocationStyle;
+    private static UiSettings mUiSettings;
 
     protected static void appearBluePot(AMap aMap){
         myLocationStyle = new MyLocationStyle();
@@ -12,11 +22,23 @@ public class FuncOfMap {
         aMap.setMyLocationStyle(myLocationStyle);
         aMap.setMyLocationEnabled(true);
         myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        myLocationStyle.showMyLocation(true);
     }
 
     protected static void appearIndoorMap(AMap aMap){
         aMap.showIndoorMap(true);
     }
 
+    protected static void changeLanguage(AMap aMap){
+        aMap.setMapLanguage("en");
+    }
 
+    protected static void appearControls(AMap aMap){
+        mUiSettings = aMap.getUiSettings();
+        mUiSettings.setZoomControlsEnabled(true);
+    }
+
+    protected static void setCenterToGuiLin(AMap aMap){
+
+    }
 }

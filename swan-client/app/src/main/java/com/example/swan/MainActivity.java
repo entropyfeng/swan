98d82e1109b61efe3a10fec8014da02b15a23a66
Devlegal <10703045@qq.com>
2019-06-05 09:21:54 +0800

    for route20190605 9:20

--- swan-client/app/src/main/java/com/example/swan/MainActivity.java
+++ swan-client/app/src/main/java/com/example/swan/MainActivity.java
@@ -5,57 +5,76 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps.AMap;
-import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.AMapOptions;
 import com.amap.api.maps.MapView;
 import com.amap.api.maps.UiSettings;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.CameraPosition;
 import com.amap.api.maps.model.LatLng;
-import com.amap.api.maps.model.Marker;
 import com.amap.api.maps.model.MarkerOptions;
+import com.amap.api.maps.model.MyLocationStyle;
+import com.amap.api.services.core.AMapException;
 import com.amap.api.services.core.LatLonPoint;
 import com.amap.api.services.core.PoiItem;
 import com.amap.api.services.core.SuggestionCity;
-import com.amap.api.services.help.Tip;
 import com.amap.api.services.poisearch.PoiResult;
 import com.amap.api.services.poisearch.PoiSearch;
-import com.example.swan.activity.Constants;
+import com.amap.api.services.route.BusRouteResult;
+import com.amap.api.services.route.DrivePath;
+import com.amap.api.services.route.DriveRouteResult;
+import com.amap.api.services.route.RideRouteResult;
+import com.amap.api.services.route.RouteSearch;
+import com.amap.api.services.route.WalkRouteResult;
+import com.example.swan.activity.LoginActivity;
 import com.example.swan.activity.MenuActivity;
 import com.example.swan.activity.SearchActivity;
-import com.example.swan.overlay.PoiOverlay;
-import com.example.swan.util.ToastUtil;
+import com.example.swan.overlay.DrivingRouteOverlay;
+import com.example.swan.route.DriveRouteDetailActivity;
+import com.example.swan.util.AMapUtil;
+import com.qmuiteam.qmui.widget.QMUIRadiusImageView;
 import com.qmuiteam.qmui.widget.QMUITopBar;
 
+import java.util.ArrayList;
 import java.util.List;
 
+public class MainActivity extends AppCompatActivity implements PoiSearch.OnPoiSearchListener, RouteSearch.OnRouteSearchListener {
 
-public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClickListener, PoiSearch.OnPoiSearchListener, AMap.InfoWindowAdapter {
+    private TextView tvResult;
 
     private MapView mapView = null;
     private AMap aMap = null;
-    private String keyWords = "";// 要输入的poi搜索关键字
-    private int currentPage;
+    private UiSettings uiSettings = null;//定义一个UiSettings对象
+
     private QMUITopBar topBar;
-    private Marker poiMarker;
     private PoiSearch poiSearch;
     private PoiSearch.Query query;
     private PoiResult poiResult;
-    private TextView keywordsTextView;
-    private ImageView cleanKeyWords;
-    private ProgressDialog progDialog = null;// 搜索时进度条
 
-    public static final int REQUEST_CODE = 100;
-    public static final int RESULT_CODE_INPUT_TIPS = 101;
-    public static final int RESULT_CODE_KEYWORDS = 102;
+    private LatLonPoint mStartPoint = new LatLonPoint(39.942295,116.335891);
+    private LatLonPoint mEndPoint = new LatLonPoint(39.995576,116.481288);
+    private RouteSearch mRouteSearch;
+    private DriveRouteResult mDriveRouteResult;
+    private final int ROUTE_TYPE_DRIVE = 2;
+    private ProgressDialog progDialog = null;//搜索时进度条
+    private RelativeLayout mBottomLayout;
+    private TextView mRouteTimeDes, mRouteDetailDes;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,16 +83,15 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClic
         getSupportActionBar().hide();
         checkPermission();
         //获取地图控件引用
-        mapView = findViewById(R.id.main_map);
+        mapView = (MapView) findViewById(R.id.main_map);
         initTopBar();
         //在activity执行onCreate时执行mapView.onCreate(savedInstanceState)，创建地图
         mapView.onCreate(savedInstanceState);
         if (aMap == null) {
             aMap = mapView.getMap();
-            setUpMap();
         }
         initMyLocation();
-        initSearch();
+        initRoute();
     }
 
     @Override
@@ -114,11 +132,6 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClic
     }
 
 
-    private void initSearch() {
-        cleanKeyWords = findViewById(R.id.main_clean_keywords);
-        cleanKeyWords.setOnClickListener(cleanKeyWordListener);
-    }
-
     private void initTopBar() {
         topBar = findViewById(R.id.main_top_bar);
         topBar.addLeftImageButton(R.drawable.ic_icon_user, 0).setOnClickListener(v -> {
@@ -126,21 +139,28 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClic
             intent.setClass(MainActivity.this, MenuActivity.class);
             startActivity(intent);
         });
-        keywordsTextView = new TextView(topBar.getContext());
+        TextView textView = new TextView(topBar.getContext());
         final String DEFAULT_LABEL_CONTENT = "查找地点、公交、地铁";
-        keywordsTextView.setOnClickListener(v -> {
+        textView.setOnClickListener(v -> {
             Intent intent = new Intent();
             intent.setClass(MainActivity.this, SearchActivity.class);
-            startActivityForResult(intent, REQUEST_CODE);
+            startActivity(intent);
         });
-        keywordsTextView.setText(DEFAULT_LABEL_CONTENT);
-        keywordsTextView.setTextColor(Color.parseColor("#9E9E9E"));
-        keywordsTextView.setHeight(80);
-        keywordsTextView.setWidth(600);
-        keywordsTextView.setGravity(Gravity.CENTER);
-        keywordsTextView.setBackgroundColor(Color.parseColor("#9E9E9E"));
-        keywordsTextView.setBackgroundResource(R.drawable.main_top_bar_text_view_border);
-        topBar.setCenterView(keywordsTextView);
+        textView.setText(DEFAULT_LABEL_CONTENT);
+        textView.setTextColor(Color.parseColor("#9E9E9E"));
+        textView.setHeight(80);
+        textView.setWidth(600);
+        textView.setGravity(Gravity.CENTER);
+        textView.setBackgroundColor(Color.parseColor("#9E9E9E"));
+        textView.setBackgroundResource(R.drawable.main_top_bar_text_view_border);
+        topBar.setCenterView(textView);
+    }
+
+    /**
+     * POI信息查询回调方法
+     */
+    @Override
+    public void onPoiSearched(PoiResult result, int rCode) {
     }
 
 
@@ -149,117 +169,90 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClic
 
     }
 
-    //检查权限
-    private void checkPermission() {
+    private void checkPermission(){
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
             //申请WRITE_EXTERNAL_STORAGE权限
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION},
-                    10086);//自定义的code
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION},
+                   10086);//自定义的code
         }
     }
 
-    /**
-     * 用marker展示输入提示list选中数据
-     *
-     * @param tip
-     */
-    private void addTipMarker(Tip tip) {
-        if (tip == null) {
-            return;
-        }
-        poiMarker = aMap.addMarker(new MarkerOptions());
-        LatLonPoint point = tip.getPoint();
-        if (point != null) {
-            LatLng markerPosition = new LatLng(point.getLatitude(), point.getLongitude());
-            poiMarker.setPosition(markerPosition);
-            aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(markerPosition, 17));
-        }
-        poiMarker.setTitle(tip.getName());
-        poiMarker.setSnippet(tip.getAddress());
+    @Override
+    public void onBusRouteSearched(BusRouteResult busRouteResult, int i) {
+
     }
 
     @Override
-    public boolean onMarkerClick(Marker marker) {
-        marker.showInfoWindow();
-        return false;
-    }
+    public void onDriveRouteSearched(DriveRouteResult result, int errorCode) {
+        dissmissProgressDialog();
+        aMap.clear();// 清理地图上的所有覆盖物
+        if (errorCode == AMapException.CODE_AMAP_SUCCESS) {
+            if (result != null && result.getPaths() != null) {
+                if (result.getPaths().size() > 0) {
+                    mDriveRouteResult = result;
+                    final DrivePath drivePath = mDriveRouteResult.getPaths().get(0);
+                    DrivingRouteOverlay drivingRouteOverlay = new DrivingRouteOverlay(
+                            this, aMap, drivePath,
+                            mDriveRouteResult.getStartPos(),
+                            mDriveRouteResult.getTargetPos(), null);
+                    drivingRouteOverlay.setNodeIconVisibility(false);//设置节点marker是否显示
+                    drivingRouteOverlay.setIsColorfulline(true);//是否用颜色展示交通拥堵情况，默认true
+                    drivingRouteOverlay.removeFromMap();
+                    drivingRouteOverlay.addToMap();
 
-    @Override
-    public View getInfoWindow(Marker marker) {
-        View view = getLayoutInflater().inflate(R.layout.poi_keyword_search_uri, null);
-        TextView title = view.findViewById(R.id.poi_keyword_search_title);
-        title.setText(marker.getTitle());
-        TextView snippet = view.findViewById(R.id.poi_keyword_search_snippet);
-        snippet.setText(marker.getSnippet());
-        return view;
-    }
+                    drivingRouteOverlay.zoomToSpan();
+                    int dis = (int) drivePath.getDistance();
+                    int dur = (int) drivePath.getDuration();
+                    String des = AMapUtil.getFriendlyTime(dur)+"("+AMapUtil.getFriendlyLength(dis)+")";
+                    mRouteTimeDes.setText(des);
+                    mRouteDetailDes.setVisibility(View.VISIBLE);
+                    int taxiCost = (int) mDriveRouteResult.getTaxiCost();
+                    mRouteDetailDes.setText("打车约"+taxiCost+"元");
+                    System.out.println("打车约"+taxiCost+"元");
+                    mBottomLayout.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent(MainActivity.this,DriveRouteDetailActivity.class);
+                            intent.putExtra("drive_path", drivePath);
+                            intent.putExtra("drive_result",mDriveRouteResult);
+                            startActivity(intent);
+                        }
+                    });
 
-    @Override
-    public View getInfoContents(Marker marker) {
-        return null;
-    }
+                } else if (result != null && result.getPaths() == null) {
+                    Toast.makeText(this,"1对不起，没有搜索到相关数据！",Toast.LENGTH_SHORT).show();
+                    System.out.println("1对不起，没有搜索到相关数据！");
+                }
 
-    /**
-     * 设置页面监听
-     */
-    private void setUpMap() {
-        aMap.setOnMarkerClickListener(this);// 添加点击marker监听事件
-        aMap.setInfoWindowAdapter(this);// 添加显示infowindow监听事件
-        aMap.getUiSettings().setRotateGesturesEnabled(false);
-    }
-
-    /**
-     * 输入提示activity选择结果后的处理逻辑
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_CODE_INPUT_TIPS && data != null) {
-            aMap.clear();
-            Tip tip = data.getParcelableExtra(Constants.EXTRA_TIP);
-            if (tip.getPoiID() == null || tip.getPoiID().equals("")) {
-                doSearchQuery(tip.getName());
             } else {
-                addTipMarker(tip);
+                Toast.makeText(this,"对不起，没有搜索到相关数据！",Toast.LENGTH_SHORT).show();
+                System.out.println("对不起，没有搜索到相关数据！");
             }
-            keywordsTextView.setText(tip.getName());
-            if (!tip.getName().equals("")) {
-                cleanKeyWords.setVisibility(View.VISIBLE);
-            }
-        } else if (resultCode == RESULT_CODE_KEYWORDS && data != null) {
-            aMap.clear();
-            String keywords = data.getStringExtra(Constants.KEY_WORDS_NAME);
-            if (keywords != null && !keywords.equals("")) {
-                doSearchQuery(keywords);
-            }
-            keywordsTextView.setText(keywords);
-            if (!keywords.equals("")) {
-                cleanKeyWords.setVisibility(View.VISIBLE);
-            }
+        } else {
+            Toast.makeText(this,String.valueOf(errorCode),Toast.LENGTH_SHORT).show();
+            System.out.println("对不起");
         }
+        System.out.println("执行完了回调");
     }
 
-    /**
-     * 开始进行poi搜索
-     */
-    protected void doSearchQuery(String keywords) {
-        showProgressDialog();// 显示进度框
-        currentPage = 1;
-        // 第一个参数表示搜索字符串，第二个参数表示poi搜索类型，第三个参数表示poi搜索区域（空字符串代表全国）
-        query = new PoiSearch.Query(keywords, "", Constants.DEFAULT_CITY);
-        // 设置每页最多返回多少条poiitem
-        query.setPageSize(10);
-        // 设置查第一页
-        query.setPageNum(currentPage);
+    @Override
+    public void onWalkRouteSearched(WalkRouteResult walkRouteResult, int i) {
 
-        poiSearch = new PoiSearch(this, query);
-        poiSearch.setOnPoiSearchListener(this);
-        poiSearch.searchPOIAsyn();
+    }
+
+    @Override
+    public void onRideRouteSearched(RideRouteResult rideRouteResult, int i) {
+
+    }
+
+    private void setfromandtoMarker() {
+        aMap.addMarker(new MarkerOptions()
+                .position(AMapUtil.convertToLatLng(mStartPoint))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.amap_start)));
+        aMap.addMarker(new MarkerOptions()
+                .position(AMapUtil.convertToLatLng(mEndPoint))
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.amap_end)));
     }
 
     /**
@@ -270,11 +263,10 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClic
             progDialog = new ProgressDialog(this);
         progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
         progDialog.setIndeterminate(false);
-        progDialog.setCancelable(false);
-        progDialog.setMessage("正在搜索:\n" + keyWords);
+        progDialog.setCancelable(true);
+        progDialog.setMessage("正在搜索");
         progDialog.show();
     }
-
 
     /**
      * 隐藏进度框
@@ -285,65 +277,35 @@ public class MainActivity extends AppCompatActivity implements AMap.OnMarkerClic
         }
     }
 
-    /**
-     * POI信息查询回调方法
-     */
-    @Override
-    public void onPoiSearched(PoiResult result, int rCode) {
-        dissmissProgressDialog();// 隐藏对话框
-        if (rCode == 1000) {
-            if (result != null && result.getQuery() != null) {// 搜索poi的结果
-                if (result.getQuery().equals(query)) {// 是否是同一条
-                    poiResult = result;
-                    // 取得搜索到的poiitems有多少页
-                    List<PoiItem> poiItems = poiResult.getPois();// 取得第一页的poiitem数据，页数从数字0开始
-                    List<SuggestionCity> suggestionCities = poiResult
-                            .getSearchSuggestionCitys();// 当搜索不到poiitem数据时，会返回含有搜索关键字的城市信息
-
-                    if (poiItems != null && poiItems.size() > 0) {
-                        aMap.clear();// 清理之前的图标
-                        PoiOverlay poiOverlay = new PoiOverlay(aMap, poiItems);
-                        poiOverlay.removeFromMap();
-                        poiOverlay.addToMap();
-                        poiOverlay.zoomToSpan();
-                    } else if (suggestionCities != null
-                            && suggestionCities.size() > 0) {
-                        showSuggestCity(suggestionCities);
-                    } else {
-                        ToastUtil.show(MainActivity.this,
-                                R.string.poi_no_result);
-                    }
-                }
-            } else {
-                ToastUtil.show(MainActivity.this,
-                        R.string.poi_no_result);
-            }
-        } else {
-            ToastUtil.showerror(this, rCode);
-        }
-
+    private void initRoute(){
+        mRouteSearch = new RouteSearch(this);
+        mRouteSearch.setRouteSearchListener(this);
+        mBottomLayout = findViewById(R.id.bottom_layout);
+        mRouteTimeDes = findViewById(R.id.firstline);
+        mRouteDetailDes = findViewById(R.id.secondline);
+        setfromandtoMarker();
+        searchRouteResult(ROUTE_TYPE_DRIVE, RouteSearch.DrivingDefault);
     }
 
-    View.OnClickListener cleanKeyWordListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            keywordsTextView.setText("");
-            aMap.clear();
-            cleanKeyWords.setVisibility(View.GONE);
-        }
-    };
-
     /**
-     * poi没有搜索到数据，返回一些推荐城市的信息
+     * 开始搜索路径规划方案
      */
-    private void showSuggestCity(List<SuggestionCity> cities) {
-        String information = "推荐城市\n";
-        for (int i = 0; i < cities.size(); i++) {
-            information += "城市名称:" + cities.get(i).getCityName() + "城市区号:"
-                    + cities.get(i).getCityCode() + "城市编码:"
-                    + cities.get(i).getAdCode() + "\n";
+    public void searchRouteResult(int routeType, int mode) {
+        if (mStartPoint == null) {
+            Toast.makeText(this, "定位中，稍后再试...", Toast.LENGTH_SHORT).show();
+            return;
         }
-        ToastUtil.show(MainActivity.this, information);
+        if (mEndPoint == null) {
+            Toast.makeText(this, "终点未设置", Toast.LENGTH_SHORT).show();
+        }
+        showProgressDialog();
+        final RouteSearch.FromAndTo fromAndTo = new RouteSearch.FromAndTo(
+                mStartPoint, mEndPoint);
+        if (routeType == ROUTE_TYPE_DRIVE) {// 驾车路径规划
+            RouteSearch.DriveRouteQuery query = new RouteSearch.DriveRouteQuery(fromAndTo, mode, null,
+                    null, "");// 第一个参数表示路径规划的起点和终点，第二个参数表示驾车模式，第三个参数表示途经点，第四个参数表示避让区域，第五个参数表示避让道路
+            mRouteSearch.calculateDriveRouteAsyn(query);// 异步路径规划驾车模式查询
+        }
 
     }
 }
